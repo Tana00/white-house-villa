b273544edcaecf3c7787610c4291cffe17fb018c
happiness-balogun <happiness.balogun@optisourcetech.com>
2023-10-03 00:23:25 +0100

    fix window undefined error

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -63,7 +63,9 @@ const apartmentPictures = [
 ];
 
 function useWindowSize() {
-  const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  if (typeof window !== "undefined") {
+    const [size, setSize] = useState([window.innerHeight, window.innerWidth]);
+  }
   useEffect(() => {
     const handleResize = () => setSize([window.innerHeight, window.innerWidth]);
     window.addEventListener("resize", handleResize);
